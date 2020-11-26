edcc3302199646208e1b3e174daaf5d36602dd6c
Jack Hubert <jphubert93@gmail.com>
2020-11-26 03:18:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,8 +11,8 @@ def game_hash
       :players => [
         {
           :player_name => "Alan Anderson",
-          :number => "0",
-          :shoe => "16",
+          :number => 0,
+          :shoe => 16,
           :points => 22,
           :rebounds => 12,
           :assists => 12,
@@ -22,8 +22,8 @@ def game_hash
         },
         {
           :player_name => "Reggie Evans",
-          :number => "30",
-          :shoe => "14",
+          :number => 30,
+          :shoe => 14,
           :points => 12,
           :rebounds => 12,
           :assists => 12,
@@ -33,8 +33,8 @@ def game_hash
         },
         {
           :player_name => "Brook Lopez",
-          :number => "11",
-          :shoe => "17",
+          :number => 11,
+          :shoe => 17,
           :points => 17,
           :rebounds => 19,
           :assists => 10,
@@ -44,8 +44,8 @@ def game_hash
         },
         {
           :player_name => "Mason Plumlee",
-          :number => "1",
-          :shoe => "19",
+          :number => 1,
+          :shoe => 19,
           :points => 26,
           :rebounds => 11,
           :assists => 6,
@@ -55,8 +55,8 @@ def game_hash
         },
         {
           :player_name => "Jason Terry",
-          :number => "31",
-          :shoe => "15",
+          :number => 31,
+          :shoe => 15,
           :points => 19,
           :rebounds => 2,
           :assists => 2,
@@ -74,8 +74,8 @@ def game_hash
       :players => [
         {
           :player_name => "Jeff Adrien",
-          :number => "4",
-          :shoe => "18",
+          :number => 4,
+          :shoe => 18,
           :points => 10,
           :rebounds => 1,
           :assists => 1,
@@ -85,8 +85,8 @@ def game_hash
         },
         {
           :player_name => "Bismack Biyombo",
-          :number => "0",
-          :shoe => "16",
+          :number => 0,
+          :shoe => 16,
           :points => 12,
           :rebounds => 4,
           :assists => 7,
@@ -96,8 +96,8 @@ def game_hash
         },
         {
           :player_name => "DeSagna Diop",
-          :number => "2",
-          :shoe => "14",
+          :number => 2,
+          :shoe => 14,
           :points => 24,
           :rebounds => 12,
           :assists => 12,
@@ -107,8 +107,8 @@ def game_hash
         },
         {
           :player_name => "Ben Gordon",
-          :number => "8",
-          :shoe => "15",
+          :number => 8,
+          :shoe => 15,
           :points => 33,
           :rebounds => 3,
           :assists => 2,
@@ -118,8 +118,8 @@ def game_hash
         },
         {
           :player_name => "Kemba Walker",
-          :number => "33",
-          :shoe => "15",
+          :number => 33,
+          :shoe => 15,
           :points => 6,
           :rebounds => 12,
           :assists => 12,
