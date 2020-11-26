a3d2d7601fc115a9e481cd188980c1377daae5ff
Jack Hubert <jphubert93@gmail.com>
2020-11-26 03:12:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,56 @@ require 'pry'
 
 def game_hash
   {
-    :home =>
-  },
-  {:away =>
+    :home => {
+      :team_name => '',
+      :colors => [
+        ],
+      :players => [
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        },
+        {
+          :player_name => "",
+          :number => "",
+          :shoe => "",
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => 
+        }]
+    },
+    :away =>
   }
 end
